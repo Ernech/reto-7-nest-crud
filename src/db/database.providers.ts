e59565ddaaa47ffbf3e008aa119f8ea31dc541ce
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-12-07 00:28:58 -0400

    Conección con la base de datos

---
+++ src/db/database.providers.ts
@@ -0,0 +1,23 @@
+
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+      name: 'postgres',
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      entities: [__dirname + '/persistence/*.entity{.ts,.js}']
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
