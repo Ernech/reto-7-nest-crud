33ef4ec73ee7bcb8305f2998778727ce1f793157
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-12-14 23:33:45 -0400

    Creating sales controller

--- src/controllers/sales/sales.controller.ts
+++ src/controllers/sales/sales.controller.ts
@@ -1,9 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { UserEntity } from 'src/db/persistence/user.entity';
+import { AuthUser } from 'src/decorators/auth-user.decorator';
+import { Authorization } from 'src/decorators/auth.decorator';
+import { Roles } from 'src/decorators/role.decorator';
+import { SaleDTO } from 'src/dto/sale.dto';
 import { SalesService } from 'src/services/sales/sales.service';
 
-@Controller('sales')
+@Controller('api/v1/sales')
 export class SalesController {
 
     constructor(private readonly salesService:SalesService){}
+
+    @Authorization()
+    @Roles("SALES_ADMIN_ROLE","SALES_USER_ROLE")
+    @Post()
+    async registerNewSale(@Body() saleDTO:SaleDTO, @AuthUser() currentUser:UserEntity){
+        return await this.salesService.createSale(saleDTO,currentUser);
+    }
+
+    @Authorization()
+    @Roles("SALES_ADMIN_ROLE","CLIENTS_ADMIN_ROLE")
+    @Get("/client/:id")
+    async getClientSale(@Param('id',new ParseIntPipe()) id:number){
+        return await this.salesService.getSalesByClientId(id);
+    }
+
+    @Authorization()
+    @Roles("SALES_ADMIN_ROLE")
+    @Delete(":id")
+    async deleteSale(@Param('id',new ParseIntPipe()) id:number, @AuthUser() currenUser:UserEntity){
+        return await this.salesService.deleteSale(id,currenUser);
+    }
+
 
 }
