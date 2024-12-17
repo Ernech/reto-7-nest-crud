d894c8d37a33e4976af6b760425ebf9251df23ff
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-12-16 22:28:45 -0400

    Integrando swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Reto 7 API')
+    .setDescription('TREST para conectar sistemas basados en HTTP para obtener y generar datos, y devolverlos en formato JSON.')
+    .setVersion('1.0')
+    .addTag('DigitalNAO')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(3000);
 }
 bootstrap();
