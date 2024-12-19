7684259d993aa6581453bbe1bb2a6c6e160457c0
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-12-18 23:51:37 -0400

    Adding ssl mode

--- src/db/database.providers.ts
+++ src/db/database.providers.ts
@@ -6,18 +6,21 @@ export const databaseProviders = [
     provide: DataSource,
     useFactory: async () => {
       const dataSource = new DataSource({
-      name: 'postgres',
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 5432,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      synchronize: true,
-      ssl:{
-        rejectUnauthorized:false
-      },
-      entities: [__dirname + '/persistence/*.entity{.ts,.js}']
+        name: 'postgres',
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        synchronize: true,
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+        entities: [__dirname + '/persistence/*.entity{.ts,.js}']
       });
 
       return dataSource.initialize();
